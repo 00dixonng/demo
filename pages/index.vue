adb14eee141eb7664cbd3f2ae3711e370170e678
Dixon Ng <dixon_ng00@hotmail.com>
2019-04-21 13:51:03 +1000

    Airstudy
    This is an Airstudy demo

---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+<v-layout>
+    <v-flex text-xs-center>
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+       <h1 class="red">Hello {{ name }}!</h1>
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+
+ 
+</template>
+
+<script>
+export default {
+  asyncData (context) {
+    // called every time before loading the component
+    // as the name said, it can be async
+    // Also, the returned object will be merged with your data object
+    return { name: 'World' }
+  },
+  fetch () {
+    // The `fetch` method is used to fill the store before rendering the page
+  },
+  head () {
+    // Set Meta Tags for this Page
+  },
+  // and more functionality to discover
+  
+}
+</script>
+
+<style>
+.red {
+  color: red;
+}
+</style>
---
+++ pages/user/_id.vue
@@ -0,0 +1,55 @@
+
+<template>
+  <h1 class="b1" v-if="myFunction() != undefined">(UserName: {{ myFunction().split("-")[0]  }} - Lastname: {{ myFunction().split("-")[1]  }} - Id: {{ myFunction().split("-")[2]  }})</h1>
+  <h1 class="b1" v-else>No User Input</h1>
+</template>
+
+<script>
+
+
+
+export default {
+
+  methods:{
+
+  myFunction: function () {
+  
+  //console.log(window.$nuxt._route.params.id==undefined);
+     
+    return window.$nuxt._route.params.id;
+    }
+	
+	},
+  
+  asyncData (context) {
+    // called every time before loading the component
+    // as the name said, it can be async
+    // Also, the returned object will be merged with your data object
+   
+    return { name:  "World"}
+  },
+  fetch () {
+    // The `fetch` method is used to fill the store before rendering the page
+  },
+ head () {
+    return {
+      // URL title
+      title: window.location.host + this.$route.fullPath,
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        // A custom description for this full path
+        { hid: 'description', name: 'description', content: 'My custom description: ' +  this.$route.fullPath}
+      ]
+    }
+  }
+  // and more functionality to discover
+  
+}
+</script>
+
+<style>
+.b1 {
+  color: black;
+  background-color: white;
+}
+</style>
